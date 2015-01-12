8a1a1864116057714f68feb58fd18a341edd02d7
Spyros Lefkaditis <slefkaditis@gmail.com>
2015-01-12 10:31:48 +0000

    Removed ad code
    Signed-off-by: Spyros Lefkaditis <slefkaditis@gmail.com>

--- header.php
+++ header.php
@@ -10,35 +10,6 @@
         <meta name="viewport" content="width=device-width">
         <link rel="shortcut icon" href="<?php echo get_stylesheet_directory_uri(); ?>/favicon.ico" />
 
-<script type='text/javascript'>
-var googletag = googletag || {};
-googletag.cmd = googletag.cmd || [];
-(function() {
-var gads = document.createElement('script');
-gads.async = true;
-gads.type = 'text/javascript';
-var useSSL = 'https:' == document.location.protocol;
-gads.src = (useSSL ? 'https:' : 'http:') + 
-'//www.googletagservices.com/tag/js/gpt.js';
-var node = document.getElementsByTagName('script')[0];
-node.parentNode.insertBefore(gads, node);
-})();
-</script>
-
-<script type='text/javascript'>
-googletag.cmd.push(function() {
-googletag.defineSlot('/37702763/IAB_728x90_Test', [728, 90], 'div-gpt-ad-1418399064729-0').addService(googletag.pubads());
-googletag.pubads().enableSingleRequest();
-googletag.enableServices();
-});
-</script>
-<script type='text/javascript'>
-googletag.cmd.push(function() {
-googletag.defineSlot('/37702763/IAB_300x250_Test', [300, 250], 'div-gpt-ad-1418398905771-0').addService(googletag.pubads());
-googletag.pubads().enableSingleRequest();
-googletag.enableServices();
-});
-</script>
         <?php wp_head(); ?>
     </head>
 	<body <?php body_class(); ?>>
